1da8e1401a0c952428657b0ebb146e0dc736bf33
Kundann123 <128457207+Kundann123@users.noreply.github.com>
2024-11-23 16:38:19 +0530

    Delete 11 directory

--- 11/src/main/kotlin/com/example/model/dto/Configurations.kt
+++
@@ -1,6 +0,0 @@
-package com.example.model.dto
-
-data class Configurations(
-    val connectionString : String,
-    val databaseName : String
-)
--- 11/src/main/kotlin/com/example/model/schema/Util.kt
+++
@@ -1,8 +0,0 @@
-package com.example.model.schema
-
-import io.ktor.server.application.*
-import io.ktor.server.auth.*
-
-
-val ApplicationCall.user get() = authentication.principal<User>()
-val testUser = User("", 1, "", "", "", listOf("Yellow", "Red"))
